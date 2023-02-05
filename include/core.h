4ea69abdb37a84f05d59bc01168112b1262a75c7
This_boy_is_nobody <i_am_nobody@gmail.com>
2023-02-05 02:09:20 +0100

    Repository initialization

---
+++ include/core.h
@@ -0,0 +1,9 @@
+#include <opencv4/opencv2/rgbd.hpp>
+#include <opencv4/opencv2/calib3d.hpp>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/common/transforms.h>
+#include <vector>
+#include <string>
+
+std::vector<cv::Mat3f>
