11838c69c24ef2b4dcd80325fbe6bbba34201a82
This_boy_is_nobody <i_am_nobody@gmail.com>
2023-05-01 19:15:08 +0200

    Removed all useless files

--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/aubin/depth_colorizer/include".split(';') if "/home/aubin/depth_colorizer/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;pcl_ros;roscpp;sensor_msgs;std_msgs;tf2;tf2_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lobject_identificator".split(';') if "-lobject_identificator" != "" else []
-PROJECT_NAME = "depth_colorizer"
-PROJECT_SPACE_DIR = "/home/aubin/depth_colorizer/build/devel"
-PROJECT_VERSION = "0.0.0"
--- build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;pcl_ros;roscpp;sensor_msgs;std_msgs;tf2;tf2_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lobject_identificator".split(';') if "-lobject_identificator" != "" else []
-PROJECT_NAME = "depth_colorizer"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
