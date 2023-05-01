20268de1644914cf669671b85628a3b2b42ce9a8
This_boy_is_nobody <i_am_nobody@gmail.com>
2023-05-02 01:08:43 +0200

    Went to creating a custom workspace to do a proper ros like workflow

--- install_ros.sh
+++ install_ros.sh
@@ -141,7 +141,7 @@ rosdep update
 sudo apt-get install ros-noetic-catkin
 sudo apt-get install python3-catkin-tools
 sudo apt-get install tree
-tree ${GITHUB_WORKSPACE}
+tree /
 #source /opt/ros/noetic/setup.bash
 echo ""
 echo "#######################################################################################################################"
